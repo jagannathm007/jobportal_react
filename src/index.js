1eb677b3d56c211090401fd639a839bd23692b4b
Jagannath Maharana <ashvinmaharana007@gmail.com>
2021-08-07 10:59:16 +0530

    Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import LoginPage from './pages/login';
+import CreatePost from './pages/createPost';
+import PostList from './pages/postsList';
+import ApplyPost from './pages/applyPost';
+import AppliedForPost from './pages/appliedForPost';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Switch>
+        <Route exact path="/" component={LoginPage}></Route>
+        <Route exact path="/posts" component={PostList}></Route>
+        <Route exact path="/create-post" component={CreatePost}></Route>
+        <Route exact path="/applied-for-post" component={AppliedForPost}></Route>
+        <Route exact path="/apply-post/:jobId" component={ApplyPost}></Route>
+        <Route path="*" children="PAGE NOT FOUND!"></Route>
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
